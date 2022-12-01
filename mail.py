983f131fddbde14f061f4e4f9f5f987a9ac05b0c
Роман Шафранюк <romanshafranuk@gmail.com>
2022-12-01 15:26:32 +0400

    Добавлена регистрация

--- mail.py
+++ mail.py
@@ -4,13 +4,41 @@ import os
 from email.mime.text import MIMEText
 import json
 
-def get_link(username:str, email:str, password:str):
-    salt = os.urandom(16)
-    with open ("key.bin", "wb") as f:
-        f.write(salt)
-    hash_passwd = hashlib.sha256(password.encode() + salt)
-    checksum = hashlib.sha3_256(hash_passwd.hexdigest().encode() + username.encode() + password.encode())
-    return f"http://localhost:5000/?username={username}&email={email}&hash_passwd={hash_passwd.hexdigest()}&data={checksum.hexdigest()}" 
+salt = "e&xKRt*kb&tUlrQ6"
+"""def get_link1(username:str, email:str, password:str, salt: str):
+    password_hash = hashlib.sha256(password.encode() + salt.encode())
+    data = username.encode() + email.encode() + password_hash.hexdigest().encode()
+    data_hash = hashlib.sha256(data + salt.encode())
+    return f"http://localhost:5000/?username={username}&email={email}&hash_passwd={password_hash.hexdigest()}&data={data_hash.hexdigest()}"
+
+def confirm_link(username:str, email:str, phash:str, dhash: str, salt: str):
+    data = username.encode() + email.encode() + phash.encode()
+    data_hash = hashlib.sha256(data + salt.encode())
+    if data_hash.hexdigest() == dhash:
+        return True
+    return False"""
+
+
+
+def get_link(username:str, email:str, password:str, salt:str):
+    #salt = os.urandom(16)
+    hash_passwd = hashlib.sha256(password.encode("utf-8") + salt.encode("utf-8"))
+    data = username.encode("utf-8") + email.encode("utf-8") + hash_passwd.hexdigest().encode("utf-8")
+    data_hash = hashlib.sha256(data + salt.encode("utf-8"))
+    return f"http://localhost:5000/confirm?username={username}&email={email}&hash_passwd={hash_passwd.hexdigest()}&data={data_hash.hexdigest()}" 
+
+def confirm_link(username:str, email:str, phash:str, dhash: str, salt: str):
+    print(f"Пользователь:{username}")
+    print(f"Email:{email}")
+    print(f"Хэш-пароль:{phash}")
+    print(f"Checksum:{dhash}")
+    print(F"Salt:{salt}")
+    data = username.encode("utf-8") + email.encode("utf-8") + phash.encode("utf-8")
+    data_hash = hashlib.sha256(data + salt.encode("utf-8"))
+    print(f"Контрольная сумма: {data_hash.hexdigest()}")
+    if data_hash.hexdigest() == dhash:
+        return True
+    return False
 
 def send_email(username:str, email:str, password:str):
     with open("mail.json","r") as f:
@@ -18,16 +46,40 @@ def send_email(username:str, email:str, password:str):
     data = json.loads(data)
     sender = data["mail"]
     passw = data["password"]
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
+    server = smtplib.SMTP_SSL("smtp.mail.ru", 465)
+    #server.starttls()
+    text = f"""
+       <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+<body>
+    <h1>Подтвердите регистрацию в dwitter!</h1>
+    <a href="{get_link(username, email, password, salt)}"><button type="button" class="btn btn-info" >Подтвердить</button></a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
+    
+    """
 
     try:
         server.login(sender, passw)
-        msg = MIMEText(get_link(username, email, password))
+        msg = MIMEText(text, "html")
         server.sendmail(sender, email, msg.as_string())
         return "The message was sent successfully!"
     except Exception as _ex:
             return f"{_ex}\nCheck your login or password please!"
 
+
+
+
 #print(get_link("shafa_01", "shafranyukroman@yandex.ru","12345678"))
-print(send_email("shafa_01", "shafranyukroman@yandex.ru","12345678"))
+#print(send_email("shafa_01", "jimmycerry999@gmail.com","12345678"))
